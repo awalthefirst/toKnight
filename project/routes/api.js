4bff8c3d6448048d0d1464084c78ac142b5066e1
Abdul   Awali <abdulrazakawal@gmail.com>
2015-12-06 12:04:18 +0000

    api

--- project/routes/api.js
+++ project/routes/api.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  
+router.get('/places', function(req, res, next) {
+  res.send("palces is here")
 });
 
 module.exports = router;
